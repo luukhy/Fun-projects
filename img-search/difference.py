abb49fe01fd69c09d9034c11b3ea1f060712c38a
Piotr Haber <hpiotrek080@gmail.com>
2025-12-20 00:13:21 +0100

    added kernel functionality

--- img-search/difference.py
+++ img-search/difference.py
@@ -4,39 +4,74 @@ import cv2
 def displayCv2(img):
     cv2.imshow('',img)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-def low_pass_filter(adata: np.ndarray, bandlimit: int = 1000, sampling_rate: int = 44100) -> np.ndarray:
-    bandlimit_index = int(bandlimit * adata.size / sampling_rate)
+def get_bbox_bfs(non_zero_idx: np.ndarray):
+    '''
+    1) choose a point from non_zero_idx
+    2) start bfs from that point 
+    3) when visiting remove from the non_zero_idx
+    4) update min_x/y and max_x/y for bbox boundries
+    5) when bfs finishes start with a not-yet-removed non_zero_index
+    6) repeat until non_zero_idx empty
+    '''
 
-    fsig = np.fft.fft2(adata)
+def convolve_3x3(in_arr: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    if kernel.shape != (3,3):
+        print("Incorrect kernel size! returning unchanged input")
+        return in_arr
+    # create multiple views of the input array so that there is no looping 
+
+    img = in_arr.astype(float)
+
+    top_left    = img[0:-2, 0:-2]
+    top_mid     = img[0:-2, 1:-1]
+    top_right   = img[0:-2, 2:] 
+
+    mid_left    = img[1:-1, 0:-2]
+    center      = img[1:-1, 1:-1]
+    mid_right   = img[1:-1, 2:]
+
+    bot_left    = img[2:, 0:-2]
+    bot_mid     = img[2:, 1:-1]
+    bot_right   = img[2:, 2:]
     
-    for i in range(bandlimit_index + 1, len(fsig) - bandlimit_index ):
-        fsig[i] = 1000
-        
-    adata_filtered = np.fft.ifft2(fsig)
+    output=(    (kernel[0][0] * top_left) + (kernel[0][1] * top_mid) + (kernel[0][2] * top_right) +
+                (kernel[1][0] * mid_left) + (kernel[1][1] * center)  + (kernel[1][2] * mid_right) +
+                (kernel[2][0] * bot_left) + (kernel[2][1] * bot_mid) + (kernel[2][2] * bot_right)
+    )
 
-    return np.real(adata_filtered)
+    output = np.abs(output)
 
-img_og = cv2.imread('G1/org.jpg')
-img_ed = cv2.imread('G1/edited.jpg')
+    output = np.clip(output, 0, 255).astype(np.uint8)
 
-diff = img_ed - img_og
-# displayCv2(diff)
+    return output
+
+# img_og = cv2.imread('G1/org.jpg').astype(float)
+# img_ed = cv2.imread('G1/edited.jpg').astype(float)
+
+img_og = cv2.imread('G2/dublin.jpg').astype(float)
+img_ed = cv2.imread('G2/dublin_edited.jpg').astype(float)
+
+# img_og = cv2.imread('G2/london.jpg').astype(float)
+# img_ed = cv2.imread('G2/london_ed.jpg').astype(float)
+
+diff = np.absolute(img_ed - img_og)
+diff = diff.astype(np.uint8)
 
 gray_scales = [0.2989, 0.5870, 0.1140]
 gray_diff = np.dot(diff[..., :3], gray_scales)
 gray_diff = gray_diff.astype(np.uint8)
-
 displayCv2(gray_diff)
-gray_filtered = low_pass_filter(gray_diff, bandlimit=10)
-gray_filtered = gray_filtered.astype(np.uint8)
 
+threshold = 0.15 *  np.max(gray_diff)
+binary_mask = gray_diff > threshold
+gray_diff = gray_diff * binary_mask
 
-gray_filtered = low_pass_filter(gray_filtered, bandlimit=10)
-gray_filtered = gray_filtered.astype(np.uint8)
+hp_kernel = np.array([  [1,  1, 1],
+                        [1, -8, 1],
+                        [1,  1, 1]])
 
-displayCv2(gray_filtered)
-gray_filtered = low_pass_filter(gray_filtered, bandlimit=10)
-gray_filtered = gray_filtered.astype(np.uint8)
+contours = convolve_3x3(gray_diff, hp_kernel)
 
-displayCv2(gray_filtered)
+non_zero = np.nonzero(contours)
