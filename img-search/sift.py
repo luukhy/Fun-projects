28ce1413e3204b9deee6b69983a06671af973b23
Piotr Haber <hpiotrek080@gmail.com>
2025-12-23 14:28:43 +0100

    optimized blurring algorithm

--- img-search/sift.py
+++ img-search/sift.py
@@ -23,7 +23,6 @@ def displayCv2(img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-@meas_time
 def get_gaussian_kerenl1d(sigma: float):
     radius = int(np.ceil(sigma * 3))
     x = np.arange(-radius, radius + 1)
@@ -33,8 +32,7 @@ def get_gaussian_kerenl1d(sigma: float):
 
     return kernel
 
-@meas_time
-def convolve_kernel1d(img: np.ndarray, kernel:np.ndarray, axis: int, mode="edge"):
+def convolve_kernel1d_slow(img: np.ndarray, kernel:np.ndarray, axis: int, mode="edge"):
     kernel_size = len(kernel)
     pad_size = kernel_size // 2
     if axis == 0:
@@ -47,33 +45,49 @@ def convolve_kernel1d(img: np.ndarray, kernel:np.ndarray, axis: int, mode="edge"
     
     return np.sum(windows * kernel, axis=-1)
 
+def convolve_kernel1d(image, kernel, batch_size=256):
+    """
+    convolves using sliding windows but processes in chunks to save memory and time
+    """
+    pad_size = len(kernel) // 2
+    pad_width = ((0, 0), (pad_size, pad_size))
+        
+    padded_image = np.pad(image, pad_width, mode='edge')
+    
+    num_rows = image.shape[0]
+    kernel_len = len(kernel)
+    
+    row_start = pad_width[0][0]     # to skip added vertical padding
+    row_end = row_start + num_rows
+    relevant_rows = padded_image[row_start:row_end, :]
+
+    windows = np.lib.stride_tricks.sliding_window_view(relevant_rows, kernel_len, axis=1)
+
+    return np.dot(windows, kernel)
+
 @meas_time
 def gaussian_blur(img:np.ndarray, sigma: float):
     kernel = get_gaussian_kerenl1d(sigma)
 
-    blurred_horizontal  = convolve_kernel1d(img, kernel=kernel, axis=1)
-    blurred_img         = convolve_kernel1d(blurred_horizontal, kernel=kernel, axis=0)
+    blurred_horizontal  = convolve_kernel1d(img, kernel=kernel)
 
-    return blurred_img
+    # rotate the og image to apply the same convolution on colums to avoid heavy math striding
+    rotated_img     = np.ascontiguousarray(blurred_horizontal.T )
+    blurred_rotated = convolve_kernel1d(rotated_img, kernel=kernel)
+    return blurred_rotated.T
 
 
 if __name__ == "__main__":
     images_paths = sorted(IMG_DIR.glob('*.jpg'))
-    images = [cv2.imread(img_path) for img_path in images_paths]
+    images = [cv2.imread(img_path).astype(np.float32) for img_path in images_paths]
 
     images = [images[0]]    # temporary to work on one picture for now
 
     for img in images:
         img = img.astype(float)
         if img.ndim == 3:
-            t = time.time()
             img = np.dot(img[..., :3], GRAYSCALE_WEIGHTS)
-            img = img.astype(np.uint8)
-            t = time.time() - t
-            print(t)
+            cv2.imwrite('out1.jpg', img.astype(np.uint8))
 
-            t = time.time()
-            blurred_img = gaussian_blur(img, 10.0)
-            t = time.time() - t
-            blurred_img = blurred_img.astype(np.uint8)
-            print(t)
+            blurred_img = gaussian_blur(img, 4)
+            cv2.imwrite('out2.jpg', blurred_img.astype(np.uint8))
